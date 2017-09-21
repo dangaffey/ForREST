555187e49ffcd44be3058f07f979c6a82de526f8
Daniel Gaffey <danielgaffey@daniels-mbp.fios-router.home>
2017-09-20 21:25:00 -0400

    Initial Commit

---
+++ forrest/forrest.h
@@ -0,0 +1,19 @@
+//
+//  forrest.h
+//  forrest
+//
+//  Created by Daniel Gaffey on 9/20/17.
+//  Copyright © 2017 UnchartedRealms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for forrest.
+FOUNDATION_EXPORT double forrestVersionNumber;
+
+//! Project version string for forrest.
+FOUNDATION_EXPORT const unsigned char forrestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <forrest/PublicHeader.h>
+
+
