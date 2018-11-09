f5183e727b0a63f9e92474f791ff2899d72bb063
Dan Gaffey <dan.gaffey@wolfe.com>
2018-11-09 11:03:12 -0500

    Updates

--- forrest/prototype/ForRESTError.swift
+++ forrest/prototype/ForRESTError.swift
@@ -13,24 +13,16 @@ open class ForRESTError {
     public var type: ForRESTErrorType
     public var httpCode: Int = -1
     public var reason: String?
+    public var error: Error?
     
-    
-    public init(_ type: ForRESTErrorType, error: Error) {
+    public init(_ type: ForRESTErrorType, response: DataResponse<Data>? = nil, error: Error? = nil) {
         self.type = type
-        self.reason = error.localizedDescription
-    }
-    
-    public init(_ type: ForRESTErrorType, response: DataResponse<Data>) {
-        self.type = type
-        self.httpCode = response.getStatusCode()
         
-        if let apiData = response.data {
+        self.httpCode = response?.getStatusCode()
+        if let apiData = response?.data {
             self.reason = String(data: apiData, encoding: .utf8)
         }
-    }
-  
-    public init(_ type: ForRESTErrorType) {
-        self.type = type
-        self.reason = type.rawValue
+        
+        self.error = error
     }
 }
