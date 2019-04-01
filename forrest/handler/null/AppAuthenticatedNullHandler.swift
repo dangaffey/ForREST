a9235c1f7a6c16519702f0c4bae7cf00dbc271b1
Dan Gaffey <dan.gaffey@wolfe.com>
2019-04-01 10:50:33 -0400

    Updates for more app auth handlers

---
+++ forrest/handler/null/AppAuthenticatedNullHandler.swift
@@ -0,0 +1,38 @@
+//
+//  AppAuthenticatedNullHandler.swift
+//  ForREST
+//
+//  Created by Dan Gaffey on 4/1/19.
+//  Copyright © 2019 UnchartedRealms. All rights reserved.
+//
+
+import Foundation
+
+
+open class AppAuthenticatedNullHandler: NullHandler {
+    
+    override open func getFailureCallback() -> (ForRESTError) -> () {
+        
+        return { (error: ForRESTError) in
+            
+            if error.httpCode == 401 || error.httpCode == 403 {
+                self.failureCallback(error)
+                NotificationCenter.default.post(name: Notifications.clearAppToken, object: nil)
+                return
+            }
+            
+            
+            switch error.type {
+            case .appAuthFailed:
+                NotificationCenter.default.post(name: Notifications.clearAppToken, object: nil)
+                
+            default:
+                break
+            }
+            
+            self.failureCallback(error)
+        }
+        
+    }
+    
+}
